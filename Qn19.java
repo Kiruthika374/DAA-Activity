03e96417489cce3395319f4128ad2302cbe15247
Kiruthika374 <rathinaswamy133@gmail.com>
2025-05-18 17:03:38 +0530

    Create Qn19.java

---
+++ Qn19.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine(); 
+        scanner.close(); 
+
+       
+        if (isPalindrome(input)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+
+   
+    private static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false; 
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
