9f05e1ef359f5ad8484b60cc8600d5f9eb2a37d0
Kiruthika374 <rathinaswamy133@gmail.com>
2025-05-18 17:09:30 +0530

    Create Qn25.java

---
+++ Qn25.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+       
+        int t = scanner.nextInt();
+        
+       
+        for (int i = 0; i < t; i++) {
+            String input = scanner.next();
+            try {
+                long number = Long.parseLong(input); // Attempt to parse as long
+                
+                System.out.printf("%s can be fitted in:%n", number);
+                
+                if (number >= Byte.MIN_VALUE && number <= Byte.MAX_VALUE) {
+                    System.out.println("* byte");
+                }
+                if (number >= Short.MIN_VALUE && number <= Short.MAX_VALUE) {
+                    System.out.println("* short");
+                }
+                if (number >= Integer.MIN_VALUE && number <= Integer.MAX_VALUE) {
+                    System.out.println("* int");
+                }
+                System.out.println("* long");
+            } catch (NumberFormatException e) {
+                System.out.printf("%s can't be fitted anywhere.%n", input);
+            }
+        }
+        
+        scanner.close();
+    }
+}
+
